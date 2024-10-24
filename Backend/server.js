93b8c50d77bc2d088b547a983909694c3cca69bc
GowthamCyro <cyrogamer.yt@gmail.com>
2024-10-24 10:44:55 +0530

    small change

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,51 @@ dotenv.config({
     path : './.env'
 })
 
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+import './src/passport.js';
+import passport from "passport";
+import session from 'express-session';
+
 import connectDB from "./src/db/index.js";
-import { app } from "./src/app.js";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+}))
+  
+app.use(
+    session({
+      secret: 'your_secret_key', 
+      resave: false,             
+      saveUninitialized: true,   
+      cookie: { secure: false }, 
+    })
+);
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/",(req,res) => {
+  res.json("Hello");
+})
+
+// import routes
+import userRouter from './src/routes/user.routes.js'
+import socailRouter from './src/routes/google.routes.js'
+
+//routes Declaration
+app.use("/api/v1/users",userRouter)
+app.use("/api/v1/socialSign",socailRouter)
 
 connectDB()
 .then(() =>{
