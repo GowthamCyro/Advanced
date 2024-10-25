6a37c699e9d42baaa8a5b0c2633eb8c0670b4cc5
GowthamCyro <cyrogamer.yt@gmail.com>
2024-10-25 14:33:49 +0530

    login

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function Login() {
+function Login({baseUrl}) {
   const navigate = useNavigate()  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
