b1730f90799c201748e7c13156bf31971f02dc76
GowthamCyro <cyrogamer.yt@gmail.com>
2024-10-28 15:33:48 +0530

    passport change

--- Backend/src/passport.js
+++ Backend/src/passport.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/socialSign/google/callback",
+      callbackURL: "https://advanced-hazel.vercel.app/api/v1/socialSign/google/callback",
     },
 
     async function (accessTokenT, refreshTokenT, profile, done) { 
