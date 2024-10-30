64f4e4abb4dab125f4f144435448e450807072b9
GowthamCyro <cyrogamer.yt@gmail.com>
2024-10-30 12:53:04 +0530

    logout samesite removed

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -312,8 +312,7 @@ const logoutUser = asyncHandler(async(req,res) => {
 
     const options = {
         httpOnly : true,
-        secure : true,
-        sameSite : "None"
+        secure : true
     }
 
     return res.
