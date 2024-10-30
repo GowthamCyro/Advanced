ec404223b190ba866287b7e74fa9dcf6cefa6fd1
GowthamCyro <cyrogamer.yt@gmail.com>
2024-10-30 15:08:37 +0530

    multer to memoryStorage

--- Backend/src/middleware/mutler.middleware.js
+++ Backend/src/middleware/mutler.middleware.js
@@ -1,12 +1,9 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination : function (req,file,cb){
-        cb(null,"./public/temp")
-    },
-    filename : function (req,file,cb) {
-        cb(null,file.originalname)
-    }
-})
+// Use memory storage
+const storage = multer.memoryStorage();
 
-export const upload = multer({storage : storage})
+const upload = multer({ storage });
+
+// Export the upload instance if needed
+export { upload };
